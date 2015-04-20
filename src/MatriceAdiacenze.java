77142f29b42e6a72526f025a689dec4e821803a0
emilioDev <acciaro70@gmail.com>
2015-04-20 20:28:55 +0200

    modificato l'algoritmo di dijkstra

--- src/MatriceAdiacenze.java
+++ src/MatriceAdiacenze.java
@@ -40,25 +40,24 @@ public class MatriceAdiacenze {
 			System.out.print("\n");
 		}
 	}
-	
-	public int[] dijkstra(int nodoR){
-		
+
+	private int[] calcSinkTreeOne(int nodoR){
 		final int nodoRadice = nodoR-1; //nodo di partenza per il calcolo del cammino minimo
 		int nextNodo = nodoR-1; //nodo che cambia per ogni riga scansionata
 		int pesoAttuale; // varibiale di controllo per il calcolo del peso minimo in ogni riga
 		int distanzaPeso = 0; //peso: counter
-		
-		
+
 		CoppiaValori[][] sinkTree = new CoppiaValori[numNodi][numNodi];
-		
+
 		for(int i=0;i<numNodi;i++){
 			for(int j=0;j<numNodi;j++){
 				sinkTree[i][j] = new CoppiaValori();
 			}
 		}
-		
-		
-		for(int j=0;j<tabAdiacenze.length;j++){
+
+		//for(int j=0;j<tabAdiacenze.length;j++){
+		int j=0;
+		while(j<tabAdiacenze.length){
 			for(int i=0;i<tabAdiacenze.length;i++){
 				if(i!=nodoRadice && i!=nextNodo){
 					if(tabAdiacenze[nextNodo][i].getPeso() != Integer.MAX_VALUE)
@@ -68,42 +67,25 @@ public class MatriceAdiacenze {
 				}
 			}
 			pesoAttuale = Integer.MAX_VALUE;
-			for(int z=0;z<numNodi;z++){
+			for(int z=0;z<tabAdiacenze.length;z++){
 				if(sinkTree[j][z].getDistanza() < pesoAttuale && sinkTree[j][z].getDistanza() > 0){
 					nextNodo = z;
 					pesoAttuale = sinkTree[j][z].getDistanza();
 				}
+
 			}
 			distanzaPeso = pesoAttuale;
+			j +=1;
 		}
-		
-		
-		//stampa provvisoria
-		
-		for(int i=0; i<numNodi; i++){
-			for(int j=0; j<numNodi; j++){
-				if(j != nodoRadice){
-					if(sinkTree[i][j].getDistanza() == Integer.MAX_VALUE)
-						System.out.print("\u221E ");
-					else
-						System.out.print(sinkTree[i][j].getDistanza()+" ");
-				}
-			}
-			System.out.print("\n");
-		}
-		
-		System.out.print("\n");
 
 		int[] valoriMinimi = new int[6];
 		int c=0;
-		int priorNodo = 0; // nodo precedente 
-		for(int j=0; j<numNodi; j++){
+		for(int g=0; g<numNodi; g++){
 			int valoreMinimo = Integer.MAX_VALUE;
-			if(j!=nodoRadice){
+			if(g!=nodoRadice){
 				for(int i=0; i<numNodi; i++){
-					if(sinkTree[i][j].getDistanza() < valoreMinimo && sinkTree[i][j].getDistanza() != 0){
-						valoreMinimo = sinkTree[i][j].getDistanza();
-						priorNodo = sinkTree[i][j].getNodoPrecedente();
+					if(sinkTree[i][g].getDistanza() < valoreMinimo && sinkTree[i][g].getDistanza() != 0){
+						valoreMinimo = sinkTree[i][g].getDistanza();
 					}
 				}
 				valoriMinimi[c] = valoreMinimo;
@@ -117,8 +99,107 @@ public class MatriceAdiacenze {
 
 		return valoriMinimi;
 	}
+
+	private int[] calcSinkTreeTwo(int nodoR){
+		final int nodoRadice = nodoR-1; //nodo di partenza per il calcolo del cammino minimo
+		int nextNodo = nodoR-1; //nodo che cambia per ogni riga scansionata
+		int pesoAttuale; // varibiale di controllo per il calcolo del peso minimo in ogni riga
+		int distanzaPeso = 0; //peso: counter
+
+		CoppiaValori[][] sinkTree = new CoppiaValori[numNodi][numNodi];
+
+		for(int i=0;i<numNodi;i++){
+			for(int j=0;j<numNodi;j++){
+				sinkTree[i][j] = new CoppiaValori();
+			}
+		}
+
+		//for(int j=0;j<tabAdiacenze.length;j++){
+		int j=0;
+		while(j<tabAdiacenze.length){
+			for(int i=0;i<tabAdiacenze.length;i++){
+				if(i!=nodoRadice && i!=nextNodo){
+					if(tabAdiacenze[nextNodo][i].getPeso() != Integer.MAX_VALUE)
+						sinkTree[j][i] = new CoppiaValori(tabAdiacenze[nextNodo][i].getPeso()+distanzaPeso,j+1);
+					else
+						sinkTree[j][i] = new CoppiaValori(tabAdiacenze[nextNodo][i].getPeso(),j+1);
+				}
+			}
+			pesoAttuale = Integer.MAX_VALUE;
+			for(int z=0;z<tabAdiacenze.length;z++){
+				if(sinkTree[j][z].getDistanza() <= pesoAttuale && sinkTree[j][z].getDistanza() > 0){
+					nextNodo = z;
+					pesoAttuale = sinkTree[j][z].getDistanza();
+				}
+
+			}
+			distanzaPeso = pesoAttuale;
+			j +=1;
+		}
+
+		//stampa provvisoria
+		stmpSinkTree(sinkTree, nodoRadice);
+
+		int[] valoriMinimi = new int[6];
+		int c=0;
+		for(int g=0; g<numNodi; g++){
+			int valoreMinimo = Integer.MAX_VALUE;
+			if(g!=nodoRadice){
+				for(int i=0; i<numNodi; i++){
+					if(sinkTree[i][g].getDistanza() < valoreMinimo && sinkTree[i][g].getDistanza() != 0){
+						valoreMinimo = sinkTree[i][g].getDistanza();
+					}
+				}
+				valoriMinimi[c] = valoreMinimo;
+				c +=1;
+			}
+			else{
+				valoriMinimi[c] = 0;
+				c +=1;
+			}
+		}
+
+		return valoriMinimi;
+	}
+
+	public int[] dijkstra(int nodoR){
+		int[] sinkTree1 = calcSinkTreeOne(nodoR);
+		int[] sinkTree2 = calcSinkTreeTwo(nodoR);
+		int counter1=0;
+		int counter2=0;
+
+		for(int i=0;i<numNodi;i++){
+			if(sinkTree1[i] < sinkTree2[i])
+				counter1++;
+			else if(sinkTree1[i] > sinkTree2[i])
+				counter2++;
+			else {
+				counter1++;
+				counter2++;
+			}
+		}
+		if(counter1 >= counter2)
+			return sinkTree1;
+		else
+			return sinkTree2;
+	}
+
+	private void stmpSinkTree(CoppiaValori[][] sinkTree,int nodoRadice){
+		for(int g=0; g<numNodi; g++){
+			for(int i=0; i<numNodi; i++){
+				if(i != nodoRadice){
+					if(sinkTree[g][i].getDistanza() == Integer.MAX_VALUE)
+						System.out.print("\u221E  ");
+					else
+						System.out.print(sinkTree[g][i].getDistanza()+"  ");
+				}
+			}
+			System.out.print("\n");
+		}
+		System.out.print("\n");
+	}
 	
-	/*
+
 	public static void main(String[] argv){
 		MatriceAdiacenze ma = new MatriceAdiacenze(6);
 		
@@ -137,7 +218,7 @@ public class MatriceAdiacenze {
 		
 		System.out.print("\n");
 
-		int[] prova = ma.dijkstra(5);
+		int[] prova = ma.dijkstra(6);
 		System.out.println(prova[0]+" "+prova[1]+" "+prova[2]+" "+prova[3]+" "+prova[4]+" "+prova[5]);
-	}*/
+	}
 }
